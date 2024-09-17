77179d37a1d1b2b0731b791938593e9409b2276a
DEYVID2000 <jdmoveisplanejados12@hotmail.com>
2024-09-16 23:52:58 -0300

    named

---
+++ named.kt
@@ -0,0 +1,11 @@
+fun printMessageWithPrefix(message: String, prefix: String) {
+    println("[$prefix] $message")
+}
+
+fun main() {
+    // Uses named arguments with swapped parameter order
+    printMessageWithPrefix(prefix = "Log", message = "Hello")
+    // [Log] Hello
+}
+
+
