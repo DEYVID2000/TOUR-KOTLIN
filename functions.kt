ec8fd283ab7ce6607bf6ee53ec688a4b458cd749
DEYVID2000 <jdmoveisplanejados12@hotmail.com>
2024-09-16 23:51:18 -0300

    functions

---
+++ functions.kt
@@ -0,0 +1,18 @@
+fun hello() {
+    return println("Hello, world!")
+}
+
+fun main() {
+    hello()
+    // Hello, world!
+}
+
+
+fun sum(x: Int, y: Int): Int {
+    return x + y
+}
+
+fun main() {
+    println(sum(1, 2))
+    // 3
+}
