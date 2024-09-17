4dca0dd950f8b523fd6b7eb70d3929847853ae89
DEYVID2000 <jdmoveisplanejados12@hotmail.com>
2024-09-16 23:47:48 -0300

    while

---
+++ while.kt
@@ -0,0 +1,27 @@
+var cakesEaten = 0
+while (cakesEaten < 3) {
+    println("Eat a cake")
+    cakesEaten++
+}
+// Eat a cake
+// Eat a cake
+// Eat a cake
+
+
+var cakesEaten = 0
+var cakesBaked = 0
+while (cakesEaten < 3) {
+    println("Eat a cake")
+    cakesEaten++
+}
+do {
+    println("Bake a cake")
+    cakesBaked++
+} while (cakesBaked < cakesEaten)
+// Eat a cake
+// Eat a cake
+// Eat a cake
+// Bake a cake
+// Bake a cake
+// Bake a cake
+
