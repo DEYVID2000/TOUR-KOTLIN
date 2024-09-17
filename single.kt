b26571f7ff3d9ccdcd8bc2675a0911e570609f6a
DEYVID2000 <jdmoveisplanejados12@hotmail.com>
2024-09-17 00:00:42 -0300

    single

---
+++ single.kt
@@ -0,0 +1,17 @@
+fun sum(x: Int, y: Int): Int {
+    return x + y
+}
+
+fun main() {
+    println(sum(1, 2))
+    // 3
+}
+
+fun sum(x: Int, y: Int) = x + y
+
+fun main() {
+    println(sum(1, 2))
+    // 3
+}
+
+
